4fd8f2186c310b8d841600a24e044415dd6e1b13
Ziggy <bladetrain3r@gmail.com>
2023-10-19 12:38:57 +0200

    Update cleanup.ps1
    Added in logic to check whether Data.p4k has been modified since last cleanup, for easier automation.

--- cleanup.ps1
+++ cleanup.ps1
@@ -1,9 +1,11 @@
-$targetPath = "C:\Program Files\Roberts Space Industries\StarCitizen"
+# Function to perform cleanup tasks based on client (LIVE or PTU)
+function PerformCleanup {
+    param (
+        [string]$client
+    )
 
-$foldersToCheck = @("PTU", "LIVE")
-
-foreach ($folder in $foldersToCheck) {
-    $userFolderPath = Join-Path -Path $targetPath -ChildPath "$folder\USER"
+    $targetPath = "C:\Program Files\Roberts Space Industries\StarCitizen"
+    $userFolderPath = Join-Path -Path $targetPath -ChildPath "$client\USER"
     
     if (Test-Path -Path $userFolderPath) {
         Write-Host "Folder $userFolderPath exists. Deleting..."       
@@ -12,14 +14,54 @@ foreach ($folder in $foldersToCheck) {
     } else {
         Write-Host "Folder $userFolderPath does not exist. Skipping."
     }
+
+    $localAppDataPath = [System.Environment]::ExpandEnvironmentVariables("%localappdata%\Star Citizen")
+
+    if (Test-Path -Path $localAppDataPath) {
+        Write-Host "Folder $localAppDataPath exists. Clearing contents..."    
+        Get-ChildItem -Path $localAppDataPath | Remove-Item -Recurse -Force    
+        Write-Host "Cleared contents of $localAppDataPath successfully."
+    } else {
+        Write-Host "Folder $localAppDataPath does not exist. Skipping."
+    }
 }
 
-$localAppDataPath = [System.Environment]::ExpandEnvironmentVariables("%localappdata%\Star Citizen")
+# Example usage for LIVE and PTU
+# PerformCleanup -client "LIVE"
+# PerformCleanup -client "PTU"
 
-if (Test-Path -Path $localAppDataPath) {
-    Write-Host "Folder $localAppDataPath exists. Clearing contents..."    
-    Get-ChildItem -Path $localAppDataPath | Remove-Item -Recurse -Force    
-    Write-Host "Cleared contents of $localAppDataPath successfully."
-} else {
-    Write-Host "Folder $localAppDataPath does not exist. Skipping."
+# Function to check Data.p4k file and take action based on client (LIVE or PTU)
+function CheckDataP4k {
+    param (
+        [string]$client,
+        [switch]$force = $false
+    )
+
+    $dataP4kPath = "C:\Program Files\Roberts Space Industries\StarCitizen\$client\Data.p4k"
+    $timestampFile = "LastDataP4kTimestamp_$client.txt"
+
+    if (Test-Path -Path $dataP4kPath) {
+        $currentTimestamp = (Get-Item $dataP4kPath).LastWriteTime.ToString("o")
+
+        if (Test-Path -Path $timestampFile) {
+            $storedTimestamp = Get-Content $timestampFile
+
+            if ($storedTimestamp -eq $currentTimestamp -and !$force) {
+                Write-Host "No new patch detected for $client. Use -force to override."
+                return
+            }
+        }
+
+        # Call PerformCleanup function
+        PerformCleanup -client $client
+
+        # Update timestamp file
+        $currentTimestamp | Out-File $timestampFile
+    } else {
+        Write-Host "Data.p4k not found for $client. Exiting."
+    }
 }
+
+# Example usage for LIVE and PTU
+CheckDataP4k -client "LIVE"
+CheckDataP4k -client "PTU" -force
